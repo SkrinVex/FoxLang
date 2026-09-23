081b429d9637e809210e14e82e0aed8c162a361e
Greb Alexey <120197437+SkrinVex@users.noreply.github.com>
2026-09-23 14:00:35 +0500

    feat(config): load .env files automatically

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,31 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <cstdlib>
 #include "Lexer.h"
 #include "Parser.h"
 
-static constexpr const char* FOX_VERSION = "5.3.0";
+static constexpr const char* FOX_VERSION = "5.4.0";
+
+static void loadDotEnv(const std::string& scriptPath) {
+    std::string dir = ".";
+    size_t slash = scriptPath.find_last_of("/\\");
+    if (slash != std::string::npos) dir = scriptPath.substr(0, slash);
+    std::ifstream env(dir + "/.env");
+    if (!env.is_open()) env.open(".env");
+    std::string line;
+    while (std::getline(env, line)) {
+        if (line.empty() || line[0] == '#') continue;
+        if (line.rfind("export ", 0) == 0) line = line.substr(7);
+        size_t eq = line.find('=');
+        if (eq == std::string::npos) continue;
+        std::string key = line.substr(0, eq), value = line.substr(eq + 1);
+        while (!key.empty() && (key.back() == ' ' || key.back() == '\t')) key.pop_back();
+        size_t ks = key.find_first_not_of(" \t"); if (ks != std::string::npos) key = key.substr(ks);
+        if (value.size() >= 2 && ((value.front() == '"' && value.back() == '"') || (value.front() == '\'' && value.back() == '\''))) value = value.substr(1, value.size()-2);
+        if (!key.empty() && std::getenv(key.c_str()) == nullptr) setenv(key.c_str(), value.c_str(), 0);
+    }
+}
 
 int main(int argc, char* argv[]) {
     if (argc == 2 && (std::string(argv[1]) == "--version" || std::string(argv[1]) == "-v")) {
@@ -26,6 +47,7 @@ int main(int argc, char* argv[]) {
     }
 
     try {
+        loadDotEnv(argv[1]);
         std::stringstream buffer;
         buffer << file.rdbuf();
 
