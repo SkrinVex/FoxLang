33ab7b0d52411a073fc80a7dbd12896912a3569a
Greb Alexey <120197437+SkrinVex@users.noreply.github.com>
2026-09-23 14:20:18 +0500

    chore: prepare runtime 5.4.2

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include "Lexer.h"
 #include "Parser.h"
 
-static constexpr const char* FOX_VERSION = "5.4.1";
+static constexpr const char* FOX_VERSION = "5.4.2";
 
 static void loadDotEnv(const std::string& scriptPath) {
     std::string dir = ".";
