479ce2859628e26234b4b981289c043cbaa6208d
Greb Alexey <120197437+SkrinVex@users.noreply.github.com>
2026-09-23 13:51:48 +0500

    chore: bump runtime to 5.2.2

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include "Lexer.h"
 #include "Parser.h"
 
-static constexpr const char* FOX_VERSION = "5.2.1";
+static constexpr const char* FOX_VERSION = "5.2.2";
 
 int main(int argc, char* argv[]) {
     if (argc == 2 && (std::string(argv[1]) == "--version" || std::string(argv[1]) == "-v")) {
